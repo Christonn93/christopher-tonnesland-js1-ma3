af74843b81d6f22be40ea8cacdf25e53b96ffbff
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-02-07 17:14:49 +0100

    starting on delivery
    Starting on the delivery for this weeks MA.

---
+++ js/question-1.js
@@ -0,0 +1,4 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f6dd075d81db416fbb289fc9d3726038";
+const apiResult = document.querySelector("#apiResult");
+
+apiResult.innerHTML = `<h1>Hello</h1>`
