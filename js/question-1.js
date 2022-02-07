aded8bdd40e95fbbb871112f58c852af2bc9c0b5
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-02-07 21:21:29 +0100

    Update question-1.js

--- js/question-1.js
+++ js/question-1.js
@@ -1,4 +1,29 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f6dd075d81db416fbb289fc9d3726038";
-const apiResult = document.querySelector("#apiResult");
+const corsUrl = "https://noroffcors.herokuapp.com/";
+const gameUrl = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f6dd075d81db416fbb289fc9d3726038";
 
-apiResult.innerHTML = `<h1>Hello</h1>`
+const URL = corsUrl + gameUrl;
+
+async function getGames() {
+  const fetchResponse = await fetch(URL);
+  const fetchResult = await fetchResponse.json();
+
+  console.log(fetchResult);
+
+  const gameTitle = fetchResult.title;
+  const gameRating = fetchResult.rating;
+  const gameTags = fetchResult.tags;
+  const gameImage = fetchResult.image_background;
+
+  const apiResult = document.querySelector("#apiResult");
+  apiResult.innerHTML += `
+    <div class="game_card">
+    <div class="ratio-box">
+    <img src="${gameImage}" class="image" alt="image missing" />
+    </div>
+    <h3> ${gameTitle} </h3>
+    <p> ${gameRating} </p>
+    <p> ${gameTags} </p>
+    </div>
+`;
+}
+getGames(apiResult);
