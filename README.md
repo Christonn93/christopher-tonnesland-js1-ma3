fcb74e2838dc7800f2a4ec7a62c55c4e8466c660
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-02-09 15:38:27 +0100

    trying to add loading
    Adding some loading.
    The creator is linked in the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # christopher-tonnesland-js1-ma3
+
+## Loading svg copied from here
+
+The loading SVG is not my work.
+
+[I found the loading svg here](https://freefrontend.com/css-loaders/)
+[The code for the Svg is from this codepen](https://codepen.io/thebabydino/pen/yjoPMJ?editors=0100)
--- js/question-2.js
+++ js/question-2.js
@@ -5,6 +5,13 @@ const URL = corsUrl + gameUrl;
 
 const apiResult = document.querySelector("#apiResult");
 
+const loadingContainer = document.querySelector(".loading");
+loadingContainer.innerHTML += `<svg viewBox="-2000 -1000 4000 2000">
+<path id="inf" d="M354-354A500 500 0 1 1 354 354L-354-354A500 500 0 1 0-354 354z"></path>
+<use xlink:href="#inf" stroke-dasharray="1570 5143" stroke-dashoffset="6713px"></use>
+</svg>`;
+
+
 setTimeout(() => {
   async function getGame() {
     const responds = await fetch(URL);
